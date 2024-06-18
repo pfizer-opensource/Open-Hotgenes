f7a6f7d7a67fbbd6422490d70de7230a9ffe310a
Rvirgenslane <Richard.Virgen-Slane@pfizer.com>
2024-06-18 10:07:34 -0700

    updated logo

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# Welcome to the Hotgenes R package repository! <img src="man/figures/logo.png" align="right" height="139" alt="" />
+# Hotgenes R package! <img src="man/figures/logo.png" align="right" height="139" alt="" />
 
 ## License information
 
--- data-raw/make_logo_magick.R
+++ data-raw/make_logo_magick.R
@@ -8,18 +8,18 @@ img <- file.path(getwd(),"data-raw", "HotgenesLogo.png")
 hexSticker_out <- file.path(getwd(), "inst", "logo", "HotgenesLogo_hexSticker.png")
 
 # ?sticker
-p<- sticker(img,
+p <- sticker(img,
   
 #h_fill = "#A9A9A9",
 # h_color = "#919191",
-h_fill = "black",
+h_fill = "#FFFB00",
 h_color = "black",
   
-h_size = 0,
+h_size = 1,
   p_y = 1, p_x = 1,
   asp = 1,
-  s_x = 1, s_y = 1.24,
-  s_width = 0.96, s_height = 0.96,
+  s_x = 1, s_y = 1.15,
+  s_width = 1.4, s_height = 1.4,
   filename = hexSticker_out, 
   package = ""
   )
@@ -27,6 +27,6 @@ h_size = 0,
 
 print(p)
 
-#?usethis::use_logo()
+# ?usethis::use_logo()
 usethis::use_logo(hexSticker_out, geometry = "240x278", retina = TRUE)
 
